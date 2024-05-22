6926e4388b38d1be1b9c6e7eb5e9e5c4049effe4
Shruti Joshi <shruti@thisissoal.com>
2024-05-22 18:11:12 +0530

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Shruthi Joshi on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
